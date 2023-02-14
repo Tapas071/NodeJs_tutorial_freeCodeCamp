b4ad4f1b46a6f3dcb70b4a12a40f67e703f324f4
Tapas Mondal <tapas710mondal@gmail.com>
2023-02-14 14:48:06 +0530

    I have watched till the end of the tutorial

--- app.cjs
+++ app.cjs
@@ -1,26 +1,11 @@
-const express = require( 'express');
+const express = require('express')
+const app = express()
 
- const app = express();
-        const {products} =require('./data.cjs')
-
-        app.get('/', (req, res)=>{
-
-            res.send('<h1> Home page </h1> <a href="/api/products"></a>')
-        })
-
-        app.get('/api/products', (req, res)=>{
-        const newProducts = products.map((product)=>{
-        const {id,name, image}=product;
-        return{id, name , image}
-
-        })
-        res.json(newProducts);
-
+app.get('/',(req,res)=>{
+    res.send('home')
 })
 
+app.listen(5000,()=>{
+    console.log('server is listening on port 5000')
+})
 
-
-
- app.listen(5000, ()=>{
-    console.log('server is listening on the port number 5000')
- })
